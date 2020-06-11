030cba7a3177b834e3d2db66e2fcdc788492b76d
Leslie <leslie.kiav@outlook.fr>
2020-06-11 02:07:33 +0200

    Revert "Revert "AjoutDesConfirmationsViews""
    This reverts commit 7c4d1d789a393d891a7ab4001f5746b95442ed16.

---
+++ Bacchus/View/ConfirmationView/AjouterSousFamilleConfirmation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bacchus.View.ConfirmationView
+{
+    public partial class AjouterSousFamilleConfirmation : Form
+    {
+        public AjouterSousFamilleConfirmation()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
