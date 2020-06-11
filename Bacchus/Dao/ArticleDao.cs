7c4d1d789a393d891a7ab4001f5746b95442ed16
Leslie <leslie.kiav@outlook.fr>
2020-06-11 02:02:48 +0200

    Revert "AjoutDesConfirmationsViews"
    This reverts commit ebf4129dba05270ab1e8dabb5b6b07344d1641a1.

--- Bacchus/Dao/ArticleDao.cs
+++ Bacchus/Dao/ArticleDao.cs
@@ -18,7 +18,7 @@ namespace Bacchus.Dao
         /// <summary>
         /// Initialise la connexion avec la Base de données "Bacchus.SQLite"
         /// </summary>
-        private static SQLiteConnection Connexion = new SQLiteConnection("Data Source= C:\\Users\\Leslie Kiav\\source\\repos\\yassine-akrafi\\Bacchus\\Bacchus\\Dao\\Bacchus.SQLite");
+        private static SQLiteConnection Connexion = new SQLiteConnection("Data Source= C:\\Users\\Lenovo\\Desktop\\Cours\\.Net\\TP\\Bacchus\\Bacchus\\Dao\\Bacchus.SQLite");
 
         /// <summary>
         /// Ajoute un article dans la base de données, 
